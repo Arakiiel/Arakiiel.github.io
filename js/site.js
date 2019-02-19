45592d8a565cc61d45d99edf2b0e08bd6c0dc61c
Callum <callum-hutchinson@live.co.uk>
2019-02-19 10:35:14 +0000

    Sticky nav in + some fixes to mobile view

--- js/site.js
+++ js/site.js
@@ -115,3 +115,28 @@ function radioButtonClickedMobile(radioButton) {
   document.getElementById("headerMobile").style.background = background;
   document.getElementById("headerMobile").style.backgroundSize = "cover";
 }
+
+window.onload = function() {
+  // When the user scrolls the page, execute myFunction
+  window.onscroll = function() {
+    myFunction();
+  };
+
+  // Get the navbar
+  var navbar = document.getElementById("navbar");
+  var headerMobileDiv = document.getElementById("headerMobile");
+
+  // Get the offset position of the navbar
+  var sticky = navbar.offsetTop;
+
+  // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+  function myFunction() {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky");
+      headerMobileDiv.classList.add("stickyAdded");
+    } else {
+      navbar.classList.remove("sticky");
+      headerMobileDiv.classList.remove("stickyAdded");
+    }
+  }
+};
